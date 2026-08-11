212248d4d57da88b55930a799dc1f4fedf41226a
andreiaballandbelloo@gmail.com <andreiaballandbelloo@gmail.com>
2026-08-10 22:02:32 -0300

    sobe exercicios

---
+++ ExercicioSeis.java
@@ -0,0 +1,43 @@
+/*
+Implemente um programa que utilize um laço do/while para ler números do usuário e verificar se são pares ou ímpares. 
+O programa deve continuar até que o usuário digite -1. Utilize o operador módulo (%) e try/catch para validação.
+*/
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExercicioSeis {
+    
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = 0;
+
+        System.out.println(" === Verifica Par ou ímpar === ");
+
+        do {
+            System.out.println("Informe um número");
+
+                try {
+                    numero = scanner.nextInt();
+
+                    if (numero == -1 ) {
+                        System.out.println("Programa encerrado.");
+                    } else if (numero % 2 == 0) {
+                        System.out.println("O número é par.");
+                    } else {
+                        System.out.println("O número é ímpar.");
+                }
+                } catch (InputMismatchException e) {
+                    System.out.println("Entrada inválida, informe um número.");
+                    scanner.nextLine();
+                    continue;
+                }
+
+        } while (numero != -1);
+
+        scanner.close();
+
+    }
+}
